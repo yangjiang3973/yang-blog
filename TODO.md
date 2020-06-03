58248a02f17c02ac43d13739049128e5ee8f16f1
IanJiang0817 <yang3973@gmail.com>
2020-06-03 22:28:08 +0800

    first setup

---
+++ TODO.md
@@ -0,0 +1,5 @@
+1. implement APIs first, and use postman to test
+
+1.1. try chinese mongodb server?
+
+2. server side rendering, and practice my css skill
---
+++ app.js
@@ -0,0 +1,5 @@
+const MongoClient = require("mongodb").MongoClient;
+const url = "mongodb://localhost:27017";
+const express = require("express");
+
+const app = express();
